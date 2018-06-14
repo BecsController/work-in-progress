001925622b44e678ee3eec1357f6c7e880b2be4b
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-06-14 22:44:00 +1200

    database and first user entry

--- db.js
+++ db.js
@@ -2,7 +2,15 @@ const knex = require('knex')
 const config = require('./knexfile').development
 const db = knex(config)
 
+function createUser(newUser) {
+  return db('users').insert(newUser)
+}
+
+function getUsers() {
+  return db('users').select()
+}
 
 module.exports = {
-  
+  createUser,
+  getUsers
 }
--- routes.js
+++ routes.js
@@ -8,4 +8,22 @@ router.get('/', (req, res) => {
   res.render('home')
 })
 
+router.post('/newProfile', (req, res) => {
+  let newUser = {
+    name: req.body.name,
+    email: req.body.email,
+    avatar: req.body.avatar
+  }
+  console.log(newUser)
+  db.createUser().then(() => {
+    res.redirect("/users")
+  })
+})
+
+router.get('/users', (req, res) => {
+  db.getUsers().then(users => {
+    res.render('users', {users})
+  })
+})
+
 module.exports = router
